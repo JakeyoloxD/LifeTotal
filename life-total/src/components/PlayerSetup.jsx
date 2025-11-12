fc07a4ea0abf0d958bdc9c66a084fbd11904219a
SelbyMalone <31752555+SelbyMalone@users.noreply.github.com>
2025-11-12 11:28:01 +1100

    Updated PlayerSetup.jsx
    Added Lifetotal to be dynamically rendered

--- life-total/src/components/PlayerSetup.jsx
+++ life-total/src/components/PlayerSetup.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import LifeCounter from "./LifeCounter";
 
 export default function PlayerSetup({ onStart }) {
   const [players, setPlayers] = useState(2);
@@ -9,27 +10,40 @@ export default function PlayerSetup({ onStart }) {
   };
 
   return (
-    <div className="setup">
-      <h2>Setup Game</h2>
+		<div>
+			<div className="setup">
+				<h2>Setup Game</h2>
 
-      <label>Number of Players:</label>
-      <input
-        type="number"
-        min="2"
-        max="6"
-        value={players}
-        onChange={(e) => setPlayers(Number(e.target.value))}
-      />
+				<label>Number of Players:</label>
+				<input
+					type="number"
+					min="2"
+					max="6"
+					value={players}
+					onChange={(e) => setPlayers(Number(e.target.value))}
+				/>
 
-      <label>Starting Life:</label>
-      <input
-        type="number"
-        min="1"
-        value={startingLife}
-        onChange={(e) => setStartingLife(Number(e.target.value))}
-      />
+				<label>Starting Life:</label>
+				<input
+					type="number"
+					min="1"
+					value={startingLife}
+					onChange={(e) => setStartingLife(Number(e.target.value))}
+				/>
 
-      <button onClick={handleStart}>Start Game</button>
-    </div>
+				<button onClick={handleStart}>Start Game</button>
+			</div>
+			<div className="board">
+				{
+					Array.from({ length:players}, (_, i) => (
+						<LifeCounter
+							key={i}
+							id={`player_${i}`}
+							startingLife = {startingLife}
+						/>
+					))
+				}
+			</div>
+		</div>
   );
 }
